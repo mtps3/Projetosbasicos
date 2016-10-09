44fa6c77aaf5506173e38dffa560acad45df5482
Allyson Thales <thaleskds@gmail.com>
2016-10-09 02:13:44 -0300

    Adicionado contador de resultados

--- descobre.sh
+++ descobre.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 #Script para encontrar diretorios e arquivos de um site utilizando uma wordlist.
-
-if [ $1 == ""]
+diretorios=0
+arquivos=0
+if [ $1 == "" ]
 then
 	echo "Script para descobrir diretorios e arquivos de um site"
 	echo "exemplo de uso: $0 www.sitealvo.com.br"
@@ -17,6 +17,7 @@ do
 if [ $resposta == 200 ]
 then
 	echo "diretorio encontrado: $palavra"
+let diretorios=$diretorios+1
 fi
 done
 	echo "##############################"
@@ -27,7 +28,20 @@ do
 	resposta=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra)
 if [ $resposta == 200 ]
 then
-	echo "Arquivo encontrado em: $palavra"
+	echo "Arquivo encontrado: $palavra"
+let arquivos=$arquivos+1
 fi
 done
+if [ $diretorios == 0 ]
+then
+echo "Não foram encontrado diretorios"
+else
+echo "Diretorios encontrado: $diretorios"
+fi
+if [ $arquivos == 0 ]
+then
+echo "Não foram encontrados arquivos"
+else
+echo "Arquivos encontrado: $arquivos"
+fi
 fi
